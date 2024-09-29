85428c017d37e4afdb8a7390308bc73732318693
rahulshend <111911437+rahulshend@users.noreply.github.com>
2024-09-29 16:08:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Python Developer", "Web Developer", "Youtuber" ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
